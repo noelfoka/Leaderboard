5727795c16c4ccc53bdcbaa63f4fe3f1d5e7439a
noelfoka <nomgnenoel@gmail.com>
2023-07-27 06:17:25 +0100

    write plain css

--- src/modules/display.js
+++ src/modules/display.js
@@ -4,32 +4,32 @@ export const score = new ScoresList();
 
 const createScore = async () => {
   const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/odai/scores',
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/odai-alazzeh/scores',
   );
   const data = await response.json();
   const { length } = data.result;
-  const listSection = document.querySelector('.results');
+  const tr = document.querySelector('.first');
+  const listSection = document.querySelector('.container');
   listSection.replaceChildren();
+  listSection.before(tr);
   if (length > 0) {
-    listSection.style.display = 'block';
-    const listContainer = document.createElement('ul');
-    listContainer.className = 'allscores';
-    listSection.appendChild(listContainer);
     for (let i = 0; i < length; i += 1) {
-      const list = document.createElement('li');
+      const list = document.createElement('tr');
       list.className = 'scoreItem';
 
-      const descrpt = document.createElement('p');
-      descrpt.id = i;
-      descrpt.textContent = `${data.result[i].user}: ${data.result[i].score}`;
+      const descrpt = document.createElement('td');
+      descrpt.textContent = `${data.result[i].user}`;
       list.appendChild(descrpt);
 
-      listContainer.append(list);
+      const descrpt1 = document.createElement('td');
+      descrpt1.textContent = `${data.result[i].score}`;
+      list.appendChild(descrpt1);
+
+      listSection.append(list);
     }
-    listSection.appendChild(listContainer);
   }
 };
-
+createScore();
 const refresh = document.getElementById('refresh');
 refresh.addEventListener('click', createScore);
 
